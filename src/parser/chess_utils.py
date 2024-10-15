e9c2eeb566184005fc1482518c2cd0083bba6d47
skanjalkar <shreyaskanjalkar@gmail.com>
2024-10-15 18:28:25 -0400

    parser for test-pgn done

--- src/parser/chess_utils.py
+++ src/parser/chess_utils.py
@@ -1,6 +1,7 @@
 import chess
 import numpy as np
 from typing import Dict, Optional
+import os
 
 PIECE_DICT: Dict[str, int] = {
     'P': 1, 'N': 2, 'B': 3, 'R': 4, 'Q': 5, 'K': 6,
@@ -24,3 +25,16 @@ def get_bin(elo: int) -> Optional[str]:
         upper_bound = lower_bound + 100
         return f"{lower_bound}-{upper_bound}"
     return None
+
+def create_bins_folders():
+    """Create the bin folders for the elo ratings.
+    Path: data/bins/{elo-bin}
+    """
+
+    for lower_bound in range(1000, 2000, 100):
+        upper_bound = lower_bound + 100
+        bin_dir = f"{lower_bound}-{upper_bound}"
+        os.makedirs(f"data/bins/{bin_dir}", exist_ok=True)
+
+    print("Bins folders created successfully.")
+    print("==================================")
--- src/parser/parser.py
+++ src/parser/parser.py
@@ -2,10 +2,13 @@ import numpy as np
 from pathlib import Path
 from game_processor import GameProcessor
 from config import PGN_FILE, DATA_DIR
-from chess_utils import get_bin
+from chess_utils import get_bin, create_bins_folders
 
 def main():
+    # down the line change the path from PGN_FILE to
+    # path from file downloaded directly from lichess
     np.random.seed(69)
+    create_bins_folders()
     processor = GameProcessor(PGN_FILE)
     for data, elo in processor.process_games():
         bin = get_bin(elo)
